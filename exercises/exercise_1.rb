f7152292063a6ade1db6e77b83e1290fd147292a
semblant <robynhumphreys7_4@hotmail.com>
2025-04-01 21:55:24 -0600

    Implement code for exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,30 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+class Store < ActiveRecord::Base
+end
+
+Store.create(
+  name: "Downtown",
+  annual_revenue: 1000000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+Store.create(
+  name: "Midtown",
+  annual_revenue: 750000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+Store.create(
+  name: "Uptown",
+  annual_revenue: 45000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+stores = Store.all
+stores.each { |store| puts store.name}
